c205dd7b7be16fb1420850e81d5b8632ebbf739f
mawi hamza <hamzamawi@hotmail.fr>
2020-10-24 12:50:52 -0100

    env access token changed

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -3,6 +3,9 @@
 require 'facebook/messenger'
 include Facebook::Messenger
 
+Facebook::Messenger::Subscriptions.subscribe(access_token: ENV["ACESS_TOKEN"])
+  
+
 Bot.on :message do |message|
   message.reply(text: 'Hello, human!')
 end
